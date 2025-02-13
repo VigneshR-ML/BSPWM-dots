5254c23b614c14d7895a1a1fc7e80febbf30157e
Vignesh <127726593+VigneshR-ML@users.noreply.github.com>
2025-02-13 22:50:30 +0530

    Update brightness.sh

--- config/bspwm/bin/brightness.sh
+++ config/bspwm/bin/brightness.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/bash
 
 send_notif(){
-	dunstctl close-all
+    dunstctl close-all
 
-	brightness=$(light -G | awk '{print int($1+0.5)}')
-	notify-send "Brightness: $brightness%" -t 1000
+    brightness=$(brightnessctl g)
+    max_brightness=$(brightnessctl m)
+    brightness=$(( brightness * 100 / max_brightness ))
+    if [ "$brightness" -le 25 ]; then
+        icon="󰃞 "  # Low brightness
+    elif [ "$brightness" -le 50 ]; then
+        icon="󰃝 "  # Medium-low brightness
+    elif [ "$brightness" -le 75 ]; then
+        icon="󰃟 "  # Medium-high brightness
+    else
+        icon="󰃠 "  # High brightness
+    fi
+    dunstify -h int:value:$brightness "$icon Brightness: $brightness%" -t 1000
 }
 
 # Execute accordingly
 case "$1" in
-	"--inc")
-		brightnessctl s +10%
-		;;
-	"--dec")
-		brightnessctl s 10%-
-		;;
-	*)
-		send_notif
-		;;
+        "--inc")
+                brightnessctl s +10%
+                ;;
+        "--dec")
+                brightnessctl s 10%-
+                ;;
+        *)
+                send_notif
+                ;;
 esac
 
 send_notif
