56dde404c7a0c5ea0d4522c84819cb1faeeddb1c
Vignesh <127726593+VigneshR-ML@users.noreply.github.com>
2025-02-13 22:51:08 +0530

    Update volume.sh

--- config/bspwm/bin/volume.sh
+++ config/bspwm/bin/volume.sh
@@ -1,25 +1,44 @@
+#!/usr/bin/bash
 
-send_notif(){
-	sink=`pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1`
-	volume=`pactl list sinks | grep '^[[:space:]]Volume:' | head -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'`
-	muted=`pactl list sinks | awk '/^\s*Mute: / {if($2=="yes"){print "!"}; exit}'`
+send_notif() {
+    dunstctl close-all
 
-	dunstctl close-all
-	notify-send "Volume: $volume" -t 1000
+    # Get the default sink (audio output)
+    sink=$(pactl get-default-sink)
 
+    # Get the volume level
+    volume=$(pactl get-sink-volume "$sink" | awk '{print $5}' | tr -d '%')
+
+    # Get the mute status
+    muted=$(pactl get-sink-mute "$sink" | awk '{print $2}')
+
+    # Select volume icon based on volume level
+    if [ "$muted" = "yes" ]; then
+        icon="󰖁 "  # Muted icon
+        volume="Muted"
+    elif [ "$volume" -le 33 ]; then
+        icon="󰕿 "  # Low volume
+    elif [ "$volume" -le 66 ]; then
+        icon="󰖀 "  # Medium volume
+    else
+        icon="󰕾 "  # High volume
+    fi
+
+    # Send notification with icon
+    dunstify -h int:value:"$volume" "$icon Volume: $volume%" -t 1000
 }
 
 # Execute accordingly
 case "$1" in
-	"--inc")
-		pactl set-sink-volume @DEFAULT_SINK@ +1%
-		;;
-	"--dec")
-		pactl set-sink-volume @DEFAULT_SINK@ -1%
-		;;
-	*)
-		send_notif
-		;;
+    "--inc")
+        pactl set-sink-volume @DEFAULT_SINK@ +1%
+        send_notif
+        ;;
+    "--dec")
+        pactl set-sink-volume @DEFAULT_SINK@ -1%
+        send_notif
+        ;;
+    *)
+        send_notif
+        ;;
 esac
-
-send_notif
